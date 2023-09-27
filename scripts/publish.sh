f268c8b809759beb3a9f29a33c665999eb4b96c9
Jake Chapman <jakechapman@Jakes-Air.lan>
2023-09-27 21:25:45 +0100

    updating publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -20,14 +20,20 @@ done
 
 # Build the Docker container
 docker build -t "$image_name" -f ./infra/docker/sybline.dockerfile .
+docker build -t "$image_name-ubi" -f ./infra/docker/sybline.dockerfile .
+
 
 # Push the container to Docker Hub if the push flag is set
 if [ "$push" = true ]; then
     docker push "$image_name"
+    docker push "$image_name-ubi"
     
     # Tag the container as "latest" if the flag is set
     if [ "$latest" = true ]; then
         docker tag "$image_name" "${image_name%:*}:latest"
+        docker tag "$image_name-ubi" "${image_name%:*}-ubi:latest"
+        
         docker push "${image_name%:*}:latest"
+        docker push "${image_name%:*}-ubi:latest"
     fi
 fi
