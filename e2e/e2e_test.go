6dca0b72ee5ced3075490e276140f0d07e14b911
Jake Chapman <jakechapman@Jakes-Air.lan>
2023-11-13 22:19:34 +0000

    simple e2e to show messages can be submitted and recieved

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -3,35 +3,113 @@ package e2e_test
 import (
 	"context"
 	"fmt"
+	"math/rand"
+	"strconv"
 	"testing"
 
+	"github.com/GreedyKomodoDragon/sybline-go/handler"
 	client "github.com/GreedyKomodoDragon/sybline-go/handler"
 	"github.com/cucumber/godog"
 )
 
-type messageBrokerCtxKey struct {
-	client client.SyblineClient
+func randomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
 }
+
+type messageBrokerCtxKey struct {
+	client   client.SyblineClient
+	queueOne string
+	queueTwo string
+	broker   string
+}
+
+var broker messageBrokerCtxKey
 
 func syblineClusterIsRunning(ctx context.Context) (context.Context, error) {
-	// Implement logic to ensure the sybline cluster is running
+	passwordManager := client.NewUnsecurePasswordManager()
+	passwordManager.SetPassword("sybline", "sybline")
+
+	cli, err := client.NewBasicSyblineClient([]string{"localhost:2221", "localhost:2222", "localhost:2223"}, passwordManager, handler.Config{
+		TimeoutSec:      5,
+		TimeoutAttempts: 3,
+	})
+	if err != nil {
+		return ctx, err
+	}
+
+	if err := cli.Login(ctx, "sybline"); err != nil {
+		return ctx, err
+	}
+
+	broker = messageBrokerCtxKey{
+		client: cli,
+	}
+
 	return ctx, nil
 }
 
-func thereIsAQueueWithNameConnectToBroker(ctx context.Context, queue, broker string) (context.Context, error) {
-	// Implement logic to create a queue and connect it to the broker
+func thereIsAQueueWithNameConnectToBroker(ctx context.Context) (context.Context, error) {
+	broker.broker = randomString(10)
+	broker.queueOne = randomString(10)
+
+	if err := broker.client.CreateQueue(ctx, broker.broker, broker.queueOne, 5, 5, false); err != nil {
+		return ctx, err
+	}
+
 	return ctx, nil
 }
 
-func messagesAreSentToBroker(ctx context.Context, messages int, broker string) (context.Context, error) {
-	// Implement logic to send messages to the specified broker
+func messagesAreSentToBroker(ctx context.Context, messages int) (context.Context, error) {
+	msgs := []client.Message{}
+	for i := 0; i < messages; i++ {
+		msgs = append(msgs, client.Message{
+			Rk:   broker.broker,
+			Data: []byte("message_" + strconv.Itoa(i)),
+		})
+	}
+
+	if err := broker.client.SubmitBatchMessage(ctx, msgs); err != nil {
+		return ctx, err
+	}
+
 	return ctx, nil
 }
 
-func consumerShouldBeAbleToGetAllMessagesFromQueue(ctx context.Context, expectedMessages int, queue string) error {
-	// Implement logic to verify the consumer gets all messages from the specified queue
-	// Use assertions or your broker's API to check this
-	fmt.Printf("Consumer gets all messages from queue %s\n", queue)
+func consumerShouldBeAbleToGetAllMessagesFromQueue(ctx context.Context, expectedMessages int) error {
+	msgs, err := broker.client.GetMessages(ctx, broker.queueOne, uint32(expectedMessages))
+	if err != nil {
+		return err
+	}
+
+	if len(msgs) != expectedMessages {
+		return fmt.Errorf("was only able to get %v messages", len(msgs))
+	}
+
+	return nil
+}
+
+func thereIsAQueueWithNameConnectToSameBroker(ctx context.Context) error {
+	broker.queueTwo = randomString(10)
+
+	return broker.client.CreateQueue(ctx, broker.broker, broker.queueTwo, 5, 5, false)
+}
+
+func consumerShouldBeAbleToGetAllMessagesFromSecondQueue(ctx context.Context, expectedMessages int) error {
+	msgs, err := broker.client.GetMessages(ctx, broker.queueTwo, uint32(expectedMessages))
+	if err != nil {
+		return err
+	}
+
+	if len(msgs) != expectedMessages {
+		return fmt.Errorf("was only able to get %v messages", len(msgs))
+	}
+
 	return nil
 }
 
@@ -41,7 +119,7 @@ func TestFeatures(t *testing.T) {
 		Options: &godog.Options{
 			Format:   "pretty",
 			Paths:    []string{"features"},
-			TestingT: t, // Testing instance that will run subtests.
+			TestingT: t,
 		},
 	}
 
@@ -52,11 +130,9 @@ func TestFeatures(t *testing.T) {
 
 func InitializeScenario(sc *godog.ScenarioContext) {
 	sc.Step(`^sybline cluster is running$`, syblineClusterIsRunning)
-	sc.Step(`^there is a queue with name "([^"]*)" connect to broker "([^"]*)"$`, thereIsAQueueWithNameConnectToBroker)
-	sc.Step(`^(\d+) messages are sent to "([^"]*)"$`, messagesAreSentToBroker)
-	sc.Step(`^consumer should be able to get all (\d+) messages from queue "([^"]*)"$`, consumerShouldBeAbleToGetAllMessagesFromQueue)
-}
-
-func main() {
-	TestFeatures(&testing.T{})
+	sc.Step(`^there is a queue connect to broker$`, thereIsAQueueWithNameConnectToBroker)
+	sc.Step(`^(\d+) messages are sent to broker$`, messagesAreSentToBroker)
+	sc.Step(`^consumer should be able to get all (\d+) messages from queue$`, consumerShouldBeAbleToGetAllMessagesFromQueue)
+	sc.Step(`^there is a another queue connect to the same broker$`, thereIsAQueueWithNameConnectToSameBroker)
+	sc.Step(`^consumer should be able to get all (\d+) messages from second queue$`, consumerShouldBeAbleToGetAllMessagesFromSecondQueue)
 }
